cd2aeae373e3938a706b35de6446b5bb2bb699a4
artslay <artslay311@gmail.com>
2026-09-21 04:39:51 +0700

    fix realpath shim newline

--- vnx/source/compat/shim_table.cpp
+++ vnx/source/compat/shim_table.cpp
@@ -213,7 +213,8 @@ static char* stub_realpath(const char* p, char* out) {
         return nullptr;
     }
 
-    struct stat st = {};\n    if (::stat(p, &st) == 0) {
+    struct stat st = {};
+    if (::stat(p, &st) == 0) {
         if (p[0] == '/') {
             strncpy(out, p, PATH_MAX - 1);
             out[PATH_MAX - 1] = '\0';
