4b585f1850bf5e5e06427c876d05d1e61b1d3e15
artslay <artslay311@gmail.com>
2026-09-22 17:44:37 +0700

    Clean up Android-style runtime configuration

--- source/config.cpp
+++ source/config.cpp
@@ -64,8 +64,6 @@ static void parse_line(char* line) {
         config.fov = std::atoi(sep);
     } else if (!std::strcmp(line, "vsync")) {
         config.vsync = std::atoi(sep);
-    } else if (!std::strcmp(line, "disable_shader_compilation")) {
-        config.disable_shader_compilation = std::atoi(sep);
     }
 }
 
@@ -80,7 +78,6 @@ int read_config(const char* path) {
     config.screen_height = 720;
     config.fov = 90;
     config.vsync = 1;
-    config.disable_shader_compilation = 0;
 
     FILE* f = std::fopen(path, "r");
     if (!f)
